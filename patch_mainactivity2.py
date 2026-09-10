2e3fbbfd7175938db34d69bab6f0d7d5d8d3dd05
Saad <khansaadibrahim@gmail.com>
2026-09-10 13:21:53 +0530

    chore: remove temporary scripting and text files from repository

--- patch_mainactivity2.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('androidApp/src/main/java/com/example/graymatter/android/ui/MainActivity.kt', 'r') as f:
-    content = f.read()
-
-content = content.replace("modifier = androidx.compose.ui.Modifier.zIndex(100f)", "modifier = androidx.compose.ui.Modifier")
-
-with open('androidApp/src/main/java/com/example/graymatter/android/ui/MainActivity.kt', 'w') as f:
-    f.write(content)
--- patch_new_entry.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open('androidApp/src/main/java/com/example/graymatter/android/ui/newentry/NewEntryScreen.kt', 'r') as f:
-    content = f.read()
-
-# I will replace the state variables with a list of states.
-# Actually, the file is 800+ lines. It's much safer to replace the Opinion Column block.
